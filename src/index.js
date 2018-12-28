a9dfee9574007db7d953b0e5069c9268bcf8d434
白文恒 <857996735@qq.com>
2018-12-28 17:01:33 +0800

    feat:新增图片压缩加载器

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './example.jpg';
 
 function component() {
   var element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], '-');
   element.classList.add('hello');
+
+  var icon = new Image();
+  icon.src=Icon;
+  element.appendChild(icon);
   return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,56 @@ module.exports = {
     path:path.resolve(__dirname, 'dist')
   },
   module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader'
-          ]
-        }
-      ]
-    }
+    rules: [
+      // 加载css
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      // 加载图片
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          // 返回data-url
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              // 当文件大于限制时使用其他加载器加载图片
+              fallback: 'image-webpack-loader'
+            },
+          },
+          // 压缩图片
+          {
+            loader: 'image-webpack-loader',
+            // 不同质量
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ]
+      }
+    ]
+  }
 }
