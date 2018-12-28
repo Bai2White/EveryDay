b1e2e8c573c66670b9c136e9afac523488495664
白文恒 <857996735@qq.com>
2018-12-28 15:54:54 +0800

    feat: css-loader style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exprots = {
   output: {
     filename: 'bundle.js',
     path:path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test:/\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 }
