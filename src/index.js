091433f68a1ad150fb0514b56555771de4deb020
白文恒 <857996735@qq.com>
2018-12-28 18:30:05 +0800

    chore:更新webpack输出方式

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './example.jpg';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
   element.innerHTML = _.join(['Hello', 'webpack'], '-');
-  element.classList.add('hello');
 
-  var icon = new Image();
-  icon.src=Icon;
-  element.appendChild(icon);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
   return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path:path.resolve(__dirname, 'dist')
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
   },
-  module: {
-    rules: [
-      // 加载css
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      // 加载图片
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          // 返回data-url
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-              // 当文件大于限制时使用其他加载器加载图片
-              fallback: 'image-webpack-loader'
-            },
-          },
-          // 压缩图片
-          {
-            loader: 'image-webpack-loader',
-            // 不同质量
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75
-              }
-            }
-          },
-        ]
-      },
-      // 加载字体
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      // 加载JSON 文件，CSV、TSV 和 XM
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
-      }
-    ]
+  output: {
+    filename: '[name].bundle.js',
+    path:path.resolve(__dirname, 'dist')
   }
 }
