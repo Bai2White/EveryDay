7afd33362590378295619b436612ab033d06c0c0
ByWhiter <whbai@86links.com>
2018-12-29 22:47:36 +0800

    #bundle

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import _ from 'lodash';
-import printMe from './print.js';
-
-function component() {
+async function getComponent() {
   var element = document.createElement('div');
-  var btn = document.createElement('button');
+  const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
   element.innerHTML = _.join(['Hello', 'webpack'], '-');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
 
   return element;
 }
-document.body.appendChild(component());
+
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,15 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: {
     app: './src/index.js',
-    another: './src/another-module.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Code Splitting'
-    })
+    }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path:path.resolve(__dirname, 'dist')
   }
 }
