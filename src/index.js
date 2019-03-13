8b8d0d613816e967e42208785e8e01a753662f0f
Bai2White <857996735@qq.com>
2019-03-13 13:11:59 +0800

    fix: 5=6

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import {cube} from './math.js'
 function component() {
   var element = document.createElement('pre');
 
-
   element.innerHTML = [
     'Hello webpack!',
-    '5 cubed is equal to s' + cube(5)
+    '6 cubed is equal to' + cube(6)
   ].join('\n\n');
 
   return element;
