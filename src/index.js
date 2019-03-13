720c4ab0465b49a06b871f97e7dde7978ce1476f
Bai2White <857996735@qq.com>
2019-03-13 13:03:03 +0800

    feat: 输入一个字

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import {cube} from './math.js'
 function component() {
   var element = document.createElement('pre');
 
+
   element.innerHTML = [
     'Hello webpack!',
-    '5 cubed is equal to' + cube(5)
+    '5 cubed is equal to s' + cube(5)
   ].join('\n\n');
 
   return element;
