5298ffcbad1fa313e4c905c3c75cc86383ba41a5
Bai2White <857996735@qq.com>
2021-12-03 13:20:30 +0800

    feat: 更新

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// import _ from 'lodash'; webpack.ProvidePlugin 已配置
-
-function component() {
-  let element = document.createElement(div);
-  element.innerHtml = join(['Hello', 'webpack'], ' ');
-  return element;
-}
-
-document.body.appendChild(component());
